0c4756491d8cd3a41bd926541777b1cd92e3dde4
chakibamine <aminechakib2003@gmail.com>
2025-10-31 23:24:32 +0100

    first commit

---
+++ src/main/java/ma/ws/jaxrs/entities/Compte.java
@@ -0,0 +1,25 @@
+package ma.ws.jaxrs.entities;
+
+import jakarta.persistence.*;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ma.ws.jaxrs.entities.TypeCompte;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlRootElement
+public class Compte {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double solde;
+    @Temporal(TemporalType.DATE)
+    private Date dateCreation;
+    @Enumerated(EnumType.ORDINAL)
+    private TypeCompte type;
+}
